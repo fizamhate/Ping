7b0fdefb8513d207a85d7873b518ced333c2abe3
Fi-za-m <134508092+Fi-za-m@users.noreply.github.com>
2025-01-23 19:24:05 +0530

    Setting up MongoDB

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Ping
 Chat App
+
+nodemon is used so changes are updated automatically. dont have to kill the job and restart the server again.
---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,14 @@
+export const signup = (req,res)=>{
+    try{
+        const {fullName,username,password,confirmPassword,gender} = req.body;
+    }
+    catch(error){
+
+    }
+}
+export const login = (req,res)=>{
+    
+}
+export const logout = (req,res)=>{
+    
+}
